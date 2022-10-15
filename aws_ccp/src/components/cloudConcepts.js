c28b6378d48b3b15976552fded94b498e64ec5ae
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2022-10-15 13:18:33 -0400

    minor changes

--- aws_ccp/src/components/cloudConcepts.js
+++ aws_ccp/src/components/cloudConcepts.js
@@ -1,5 +1,9 @@
 // Import React //
 import React from 'react';
+// Import react Icons
+import { 
+    DiAws
+} from "react-icons/di";
 
 const cloudConcepts = () => {
     return (
@@ -8,6 +12,14 @@ const cloudConcepts = () => {
                 <h2>Cloud Concepts</h2>
                 <h6 className="info">This section of the exam will be worth&nbsp;<strong>26%</strong>&nbsp;of your total score</h6>
             </div>    
+            <div className="deploymentModels">
+            {/* <DiAws className="icon display-4" title="aws_icon"/> */}
+                <ul>The Deployment Models of the Cloud</ul>
+                    <li>Private Cloud</li>
+                    <li>Public Cloud</li>
+                    <li>Hybrid Cloud</li>
+            
+            </div>
         </div>
     )
 }
